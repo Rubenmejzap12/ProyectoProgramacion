c6285434e4974442b881a7dadba7f16e2be2600f
Ruben Mejías <rmejzap999@g.educaand.es>
2022-01-24 10:35:31 +0100

    Revisión7 | RMZ. Creacion documentos xml, xml_X, dtd y xsd de concesionario

--- src/main/java/es/rubenmejias/projectfinal_ruben/App.java
+++ src/main/java/es/rubenmejias/projectfinal_ruben/App.java
@@ -7,7 +7,7 @@ import javafx.event.ActionEvent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
@@ -18,36 +18,51 @@ import javafx.util.Duration;
  */
 public class App extends Application {
     
-    int fondo1X = 0;
-    int fondo2X = 800;
+    
+    int PantallaX = 800;
+    int PantallaY = 450;
     ImageView fondo1;
     ImageView fondo2;
+    int fondo1X = 0;
+    int fondo2X = 800;
+    ImageView personaje1;
+    int personajeX = 100;
+    int personajeY = 0;
     
     
     @Override
     public void start(Stage stage) {
-        StackPane root = new StackPane();
-        Scene scene = new Scene (root, 800, 450);
+        Pane root = new Pane();
+        Scene scene = new Scene (root, PantallaX, PantallaY);
         stage.setTitle("BosqueMaldito");
         stage.setScene(scene);
         stage.show();
-        fondo1 = new ImageView();
-        fondo2 = new ImageView();
+        
+        //Imagen fondo
+        Image fondoImg = new Image (getClass().getResourceAsStream("/images/suelo1.jpg"));
+        fondo1 = new ImageView(fondoImg);
         root.getChildren().add(fondo1);
+        fondo2 = new ImageView(fondoImg);
         root.getChildren().add(fondo2);
+        
+        
         fondo1.setLayoutX(fondo1X);
         fondo2.setLayoutX(fondo2X);
-        // Cargar la imagen crear objeto ImageView
-        Image img = new Image(getClass().getResourceAsStream(""));
-        fondo1 = new ImageView(img);
-        root.getChildren().add(fondo1);
-        fondo2 = new ImageView(img);
-        root.getChildren().add(fondo2);
-       
+        
+        
+        //Imagen personaje
+        Image personajeImg = new Image (getClass().getResourceAsStream("/images/personaje.gif"));
+        personaje1 = new ImageView(personajeImg);
+        root.getChildren().add(personaje1);
+        
+        personaje1.setLayoutX(personajeX);
+        personaje1.setLayoutX(personajeY);
         //Desplazar la pantala ala izquierda
         Timeline fondoScroll = new Timeline(
-                  new KeyFrame(Duration.seconds(0.007), (ActionEvent ae) -> {
+                  new KeyFrame(Duration.seconds(0.017), (ActionEvent ae) -> {
+                      System.out.println("dentro del timeline");
                       fondo1X = fondo1X -1;
+                      System.out.println(fondo1X);
                       fondo1.setLayoutX(fondo1X);
                       fondo2X = fondo2X -1;
                       fondo2.setLayoutX(fondo2X);
@@ -61,11 +76,6 @@ public class App extends Application {
           fondoScroll.setCycleCount(Timeline.INDEFINITE);
           fondoScroll.play(); // EJECUTAR EL TIMELINE
           
-                  // Cargar la imagen crear objeto ImageView
-        Image img = new Image(getClass().getResourceAsStream("/images/suelo1.jpg"));
-        ImageView imgView = new ImageView(img);
-        // Añadir el ImageView al panel principal de la pantalla 
-        paneRoot.getChildren().add(imgView);
     }
 
     public static void main(String[] args) {
