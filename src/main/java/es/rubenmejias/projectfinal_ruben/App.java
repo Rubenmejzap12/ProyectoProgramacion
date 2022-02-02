69ca36da81006f7d49c7a33e8a226c0573c5677d
Ruben Mejías <rmejzap999@g.educaand.es>
2022-02-02 10:31:40 +0100

    Revisión 5 | RMZ. Creacion texto puntuaciones y inicio de colision con oveja

--- src/main/java/es/rubenmejias/projectfinal_ruben/App.java
+++ src/main/java/es/rubenmejias/projectfinal_ruben/App.java
@@ -4,13 +4,20 @@ import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
+import javafx.geometry.Pos;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import static javafx.scene.input.KeyCode.DOWN;
 import static javafx.scene.input.KeyCode.UP;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
@@ -20,7 +27,8 @@ import javafx.util.Duration;
  * JavaFX App
  */
 public class App extends Application {
-    
+    int score;
+    int highScore;
     
     int PantallaX = 800;
     int PantallaY = 400;
@@ -32,19 +40,90 @@ public class App extends Application {
     int aguilaX = 0;
     int aguilaY = 0;
     int stickCurrentSpeed = 0;
+    ImageView oveja1;
+    int ovejaX = 0;
+    int ovejaY = 0;
+    
+    
+    final int TEXT_SIZE = 24;
+    Text textScore;
+    Pane root = new Pane();
     
     @Override
     public void start(Stage stage) {
         Pane root = new Pane();
-        Scene scene = new Scene (root, PantallaX, PantallaY);
-        stage.setTitle("AguilaDepredadora");
+        Scene scene = new Scene (root, PantallaX, PantallaY); // Creacion ventana juego con las medidas de ancho y alto
+        stage.setTitle("AguilaDepredadora"); //Titulo que aparece en ventana
         stage.setScene(scene);
         stage.show();
+       
+        
+        
+        
+         //LAYOUTS PARA MOSTRAR PUNTUACIONES
+
+          //Layout principal
+          HBox paneScores = new HBox();
+          paneScores. setTranslateY(20) ;
+          paneScores. setMinWidth(PantallaX) ;
+          paneScores. setAlignment (Pos.CENTER) ;
+          paneScores. setSpacing(100) ;
+          root.getChildren().add(paneScores) ;
+
+          //Layout para puntuación actual
+          HBox paneCurrentScore = new HBox();
+          paneCurrentScore. setSpacing(10) ;
+          paneScores.getChildren().add(paneCurrentScore) ;
+
+          //Layout para puntuación maxima
+          HBox paneHighScore = new HBox();
+          paneHighScore. setSpacing(10) ;
+          paneScores.getChildren().add(paneHighScore) ;
+
+          //Texto de etiqueta para la puntuación
+          Text textTitleScore = new Text("Score:");
+          textTitleScore.setFont(Font.font(TEXT_SIZE));
+          textTitleScore.setFill(Color.BLACK);
+
+          //Texto para la puntuacion
+          Text textScore = new Text("0");
+          textScore. setFont (Font. font (TEXT_SIZE));
+          textScore. setFill(Color.BLACK) ;
+
+          //Texto de etiqueta para la puntuación maxima
+          Text textTitleHighScore = new Text("Max.Score:");
+          textTitleHighScore. setFont (Font. font (TEXT_SIZE));
+          textTitleHighScore.setFill(Color.BLACK) ;
+
+          //Texto para la puntuación maxima
+          Text textHighScore = new Text("0");
+          textHighScore. setFont (Font. font (TEXT_SIZE)) ;
+          textHighScore. setFill(Color.BLACK) ;
+          paneCurrentScore.getChildren().add(textTitleScore) ;
+          paneCurrentScore.getChildren().add(textScore) ;
+          paneHighScore.getChildren().add(textTitleHighScore);
+          paneHighScore.getChildren().add(textHighScore);
+
+          
+        
+        
+        // Imagen 1
+        ImageView oveja1 = new ImageView();
+        Rectangle rectZona1 = new Rectangle(226, 138); // Debe englobar a la imagen
+        Group groupZona1 = new Group();
+        groupZona1.getChildren().addAll(oveja1, rectZona1);
+        // Hacer invisible el rectángulo
+        rectZona1.setVisible(false);
+ 
+        
+        
         
         //Imagen fondo
         Image fondoImg = new Image (getClass().getResourceAsStream("/images/pradera.jpg"));
         fondo1 = new ImageView(fondoImg);
         root.getChildren().add(fondo1);
+        
+        //Imagen 2 del fondo que hemos añadido siguiendo a la 1 para el movimiento el fondo
         Image fondoImg1 = new Image (getClass().getResourceAsStream("/images/pradera2.jpg"));
         fondo2 = new ImageView(fondoImg1);
         root.getChildren().add(fondo2);
@@ -59,12 +138,18 @@ public class App extends Application {
         
         aguila1.setLayoutX(aguilaX);
         aguila1.setLayoutY(aguilaY);
+        
+        
+        
+        
+        
 
-        //Desplazar la pantala ala izquierda
+        //Desplazar la pantala a la izquierda para que paresca infinita
         Timeline fondoScroll = new Timeline(
-                  new KeyFrame(Duration.seconds(0.004), (ActionEvent ae) -> {
+                  new KeyFrame(Duration.seconds(0.006), (ActionEvent ae) -> {
                       fondo1X = fondo1X -1;
                       fondo1.setLayoutX(fondo1X);
+                      
                       fondo2X = fondo2X -1;
                       fondo2.setLayoutX(fondo2X);
                       if (fondo1X == -800) {
@@ -75,9 +160,14 @@ public class App extends Application {
                   })
         
           );
-        //Movimiento personaje con teclas
+        //EJECUTA MOVIMIENTO FONDO
+        fondoScroll.setCycleCount(Timeline.INDEFINITE);
+        fondoScroll.play(); // EJECUTAR EL TIMELINE
+        
+        
+        //Movimiento personaje con las teclas deseadas
         Timeline aguilaScroll = new Timeline(
-                  new KeyFrame(Duration.seconds(0.017), (ActionEvent ae) -> {
+                  new KeyFrame(Duration.seconds(0.005), (ActionEvent ae) -> {
                       scene.setOnKeyPressed((KeyEvent event) -> {
                       aguila1.setLayoutX(aguilaX);
                       aguila1.setLayoutY(aguilaY);
@@ -95,7 +185,7 @@ public class App extends Application {
                           }
                         break;
                       case RIGHT:
-                        //PULSADA TECLA DERECHA
+                        //PULSADA TECLA DERECHA y que no se salga el personaje por el lateral derecho
                         if (aguilaX <= 540){
                         aguilaX = aguilaX +3;
                           }
@@ -111,16 +201,15 @@ public class App extends Application {
                   })
         
           );
-          //EJECUTA MOVIMIENTO FONDO
-          fondoScroll.setCycleCount(Timeline.INDEFINITE);
-          fondoScroll.play(); // EJECUTAR EL TIMELINE
-          //EJECUTA MOVIMIENTO AGUILA
-          aguilaScroll.setCycleCount(Timeline.INDEFINITE);
-          aguilaScroll.play(); // EJECUTAR EL TIMELINE
+        //EJECUTA MOVIMIENTO AGUILA
+        aguilaScroll.setCycleCount(Timeline.INDEFINITE);
+        aguilaScroll.play(); // EJECUTAR EL TIMELINE
           
     }
 
     public static void main(String[] args) {
         launch();
     }
+    
+  
 }
